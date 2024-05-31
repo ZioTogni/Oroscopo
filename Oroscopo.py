90558c76ae63dbae42c29eab23c2ef57c9e2fda1
Manuel Favagrossa <159720066+ZioTogni@users.noreply.github.com>
2024-05-31 23:30:17 +0200

    Add files via upload

---
+++ Oroscopo.py
@@ -0,0 +1,57 @@
+#VARIABILI
+nome_assistente="Pietro"
+mese_1="Gennaio"
+mese_2="Febbraio"
+mese_3="Marzo"
+mese_4="Aprile"
+mese_5="Maggio"
+mese_6="Giugno"
+mese_7="Luglio"
+mese_8="Agosto"
+mese_9="Settembre"
+mese_10="Ottobre"
+mese_11="Novembre"
+mese_12="Dicembre"
+#PROGRAMMA
+print("Ciao mi chiamo "+nome_assistente+" o così mi chiamano!")
+print("\nSono qui per dirti chi sei in base al tuo mese di nascita che tu mi dirai!")
+print("\nOra dimmi il mese in cui sei nato!")
+mese_di_nascita=input()
+if mese_di_nascita==mese_1:
+    print("Fantastico sei nato nel mese di "+mese_di_nascita+"!")
+    print("Se sei nato a Gennaio sei come una montagna, forte, ambizioso e pronto a scalare le vette della vita!")
+elif mese_di_nascita==mese_2:
+    print("Fantastico sei nato nel mese di "+mese_di_nascita+"!")
+    print("Se sei nato a Febbraio sei sempre pieno di idee originali e fuori schemi, continua così!")
+elif mese_di_nascita==mese_3:
+    print("Fantastico sei nato nel mese di "+mese_di_nascita+"!")
+    print("Se sei nato a Marzo sei come i sogni: pieno di emozioni, molto bene!")
+elif mese_di_nascita==mese_4:
+    print("Fantastico sei nato nel mese di "+mese_di_nascita+"!")
+    print("Se sei nato ad Aprile sei come un razzo: sempre pronto a partire con una scia di entusiasmo!")
+elif mese_di_nascita==mese_5:
+    print("Fantastico sei nato nel mese di "+mese_di_nascita+"!")
+    print("Se sei nato a Maggio sei come un albero gigante: radicato nella terra, ma con rami che si estendono verso il cielo!")
+elif mese_di_nascita==mese_6:
+    print("Fantastico sei nato nel mese di "+mese_di_nascita+"!")
+    print("Se sei nato a Giugno sei come un paio di cuffie: sempre in movimento e pronto a cambiare canzone!")
+elif mese_di_nascita==mese_7:
+    print("Fantastico sei nato nel mese di "+mese_di_nascita+"!")
+    print("Se sei nato a Luglio sei come una conchiglia: protettivo e pieno di segreti!")
+elif mese_di_nascita==mese_8:
+    print("Fantastico sei nato nel mese di "+mese_di_nascita+"!")
+    print("Se sei nato ad Agosto sei come il Sole: luminoso e sempre al centro dell'attenzione!")
+elif mese_di_nascita==mese_9:
+    print("Fantastico sei nato nel mese di "+mese_di_nascita+"!")
+    print("Se sei nato a Settembre sei come una bilancia: sempre alla ricerca di un equilibrio perfetto")
+elif mese_di_nascita==mese_10:
+    print("Fantastico sei nato nel mese di "+mese_di_nascita+"!")
+    print("Se sei nato a Ottobre sei come un detective: sempre alla ricerca della verità nascosta!")
+elif mese_di_nascita==mese_11:
+    print("Fantastico sei nato nel mese di "+mese_di_nascita+"!")
+    print("Se sei nato a Novembre sei come un arciere: mira in alto e colpisce sempre il bersaglio!")
+elif mese_di_nascita==mese_12:
+    print("Fantastico sei nato nel mese di "+mese_di_nascita+"!")
+    print("Se sei nato a Dicembre sei come una montagna: solido, ambizioso e con una vista panoramica sulla vita!")
+else:
+    print("Questo non è un mese di nascita, riprova riavviando il programma!")
